8ebb279bb2b5e8be480b41b3493b3c5ff231dee1
riyanthi.ng <110769715+riyanthi25@users.noreply.github.com>
2023-03-09 15:15:27 +0800

    hapus title

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,9 @@ import About from "../components/UI/About"
 import Portfolio from "../components/UI/Portfolio"
 import Skills from "../components/UI/Skills"
 import Contact from "../components/UI/Contact"
-import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <Head>
-        <title>My Portfolio</title>
-      </Head>
-    </div>
   <Fragment>
     <Hero />
     <About />
