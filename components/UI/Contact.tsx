93e0473438b1a618b889d6ebf833ee46d7ffbb0c
putu.riyanthi.ng@gmail.com <putu.riyanthi.ng@gmail.com>
2026-09-08 10:14:59 +0800

    change phone

--- components/UI/Contact.tsx
+++ components/UI/Contact.tsx
@@ -22,19 +22,19 @@ const Contact = () => {
                 <span>
                   <i className="ri-map-pin-line"></i>
                 </span>
-                <Link className={`${classes.info__link}`} target="_blank" href="https://www.google.com/maps/place/Jl.+Mekar+I+No.24,+Pemogan,+Denpasar+Selatan,+Kota+Denpasar,+Bali+80221/@-8.7099976,115.2014326,17z/data=!3m1!4b1!4m6!3m5!1s0x2dd241385f4fa353:0x8c46ededbc9fb91b!8m2!3d-8.7100029!4d115.2036213!16s%2Fg%2F11c1zj93m3" >Jl. Mekar 1 No.24 Pemogan, South Denpasar, Indonesia</Link>
+                <Link className={`${classes.info__link}`} target="_blank" href="" >Denpasar, Bali</Link>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-mail-line"></i>
                 </span>
-                <Link className={`${classes.info__link}`} target="_blank" href="https://mail.google.com/mail/u/0/?view=cm&tf=1&fs=1&to=putu.riyanthi.ng@gmail.com">putu.riyanthi.ng@gmail.com</Link>
+                <Link className={`${classes.info__link}`} target="_blank" href="">hanjin.riyanthi.ng@gmail.com</Link>
               </li>
               <li className={`${classes.info__item}`}>
                 <span>
                   <i className="ri-phone-line"></i>
                 </span>
-                <Link className={`${classes.info__link}`} target="_blank" href="https://wa.me/+6285737005452?text=Hello!,%20I%20have%20something%20to%20talk%20to%20you%20about....">+6285737005452</Link>
+                <Link className={`${classes.info__link}`} target="_blank" href="https://wa.me/+6285737005452?text=Hello!,%20I%20have%20something%20to%20talk%20to%20you%20about....">+628216290871</Link>
               </li>
             </ul>
 
